aa46060ae887ec21762e510d144b766c88e809a7
Mathieu Dion <mathieu.dion98@gmail.com>
2021-06-09 00:09:56 -0400

    added first table 'employee

---
+++ test/system/employees_test.rb
@@ -0,0 +1,49 @@
+require "application_system_test_case"
+
+class EmployeesTest < ApplicationSystemTestCase
+  setup do
+    @employee = employees(:one)
+  end
+
+  test "visiting the index" do
+    visit employees_url
+    assert_selector "h1", text: "Employees"
+  end
+
+  test "creating a Employee" do
+    visit employees_url
+    click_on "New Employee"
+
+    fill_in "email", with: @employee.Email
+    fill_in "first name", with: @employee.First_name
+    fill_in "last name", with: @employee.Last_Name
+    fill_in "title", with: @employee.Title
+    click_on "Create Employee"
+
+    assert_text "Employee was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Employee" do
+    visit employees_url
+    click_on "Edit", match: :first
+
+    fill_in "email", with: @employee.Email
+    fill_in "first name", with: @employee.First_name
+    fill_in "last name", with: @employee.Last_Name
+    fill_in "title", with: @employee.Title
+    click_on "Update Employee"
+
+    assert_text "Employee was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Employee" do
+    visit employees_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Employee was successfully destroyed"
+  end
+end
