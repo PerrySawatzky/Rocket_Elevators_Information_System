31bb63ddec9af971e7d1ac4892a0a3525baaf271
Mathieu Dion <mathieu.dion98@gmail.com>
2021-06-09 19:17:51 -0400

    nothing much done

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2021_06_08_175226) do
 
- 
+  create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_Name"
+    t.string "title"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
