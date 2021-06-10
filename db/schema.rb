872e94f6175eb3ea0e07373140c0250393093908
PerrySawatzky <sawatzky.perry@gmail.com>
2021-06-09 21:44:59 -0400

    added commercial page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_175226) do
+ActiveRecord::Schema.define(version: 2021_06_09_210003) do
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "First_name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2021_06_08_175226) do
     t.datetime "updated_at", null: false
   end
 
-ActiveRecord::Schema.define(version: 2021_06_09_210003) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
