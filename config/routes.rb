17444422911d3f164a40b8c9f67c5b878e74a001
PerrySawatzky <sawatzky.perry@gmail.com>
2021-06-11 09:32:47 -0400

    tied employee to user

--- config/routes.rb
+++ config/routes.rb
@@ -10,3 +10,19 @@ Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+  
+# 	post "/home/testpost", to: "home#testpost"
+# 	get "/index", to: "rocket#index"
+# 	get "/quote", to: "rocket#quote"
+# 	get "/residential", to: "rocket#residential"
+# 	get "/commercial", to: "rocket#commercial"
+
+# 	mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+# 	resources :employees
+# 	get 'home/index'
+# 	get 'home_controller/index'
+# 	devise_for :users     
+
+# 	post "/quotes/create", to: "quotes#create"
+# end
