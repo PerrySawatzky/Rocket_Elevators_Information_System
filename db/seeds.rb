7af510f29e6ab4f1eb58010479da9960bfee6429
PerrySawatzky <sawatzky.perry@gmail.com>
2021-06-17 20:39:07 -0400

    improved the BI sync page

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,7 @@ data_hash = JSON.parse(file)
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-<<<<<<< HEAD
 require "faker"
-=======
- require "faker"
->>>>>>> a7780d6b32c2edc44bec3d9944de3edbc8810fa6
 
 
 employees_list = [
@@ -87,7 +83,7 @@ address_status = ["Active", "Inactive"]
 adress_entity = ["Building", "Customer"]
 i = 0
 data_hash['addresses'].each do |address|  
-  Address.create(type_of_address: address_type[rand(4)], status: address_status[rand(2)],entity: adress_entity[rand(2)], number_and_street: address['address1'], suite_or_apartment: address['address2'], city: address['city'], postal_code: address['postalCode'], country: address["state"], notes: Faker::Lorem.paragraph  )
+  Address.create(address_type: address_type[rand(4)], status: address_status[rand(2)],entity: adress_entity[rand(2)], number_and_street: address['address1'], suite_or_apartment: address['address2'], city: address['city'], postal_code: address['postalCode'], country: address["state"], notes: Faker::Lorem.paragraph  )
   
 
   Lead.create(
--- lib/BI/sync.rb
+++ lib/BI/sync.rb
@@ -13,52 +13,99 @@ module BI
                 :user => 'officeimac', 
                 :password => '' )
                 self.conn = conn
-        end
-
-        def query_dim_customers
-            self.conn.exec( "SELECT * FROM DimCustomers" ) do |result|
-                #select answers from this table and for each, make one result
-                # creation = created_at
-                # company_name = company_name
-                # full_name_company_contact = company_contact_full_name
-                # email_company_contact = company_contact_email
-                # num_of_elevators = num entries in elevators table with user_id
-                
-                result.each do |row|
-                    #for each result do a row
-                    puts row.values_at('company_name')
-                    #look at the column with value 'company_name' and print those
-                    #INSERT into DimCustomers (company_name) VALUES ('PARROT INC')
-                end
             end
-        end
-        def query_facts_contact
-            #contact_id
-            #creation
-            #company_name
-            #email
-            #project_name
-            self.conn.exec( "SELECT * FROM ")
-        end
-
-        def query_facts_quotes
-            Quotes.all.each do |quotes|
+            
+        def query_fact_quotes
+            Quote.all.each do |quotes|
+                #quote_id > Quote > id
+                #creation > Quote > created_at
+                #company_name > ?????
+                #email > ????
+                # nbElevator > Quote > elevator_amount
                 puts "hello"
             end
-                # quotes.each do |row|
-                #     row.values_at('')
+            # quotes.each do |row|
+            #     row.values_at('')
         end
-        # customers in postrgres vs customers in mysql
-        # creation = created_at
-        # company_name = company_name
-        # full_name_company_contact = company_contact_full_name
-        # email_company_contact = company_contact_email
-        # num_of_elevators = num entries in elevators table with user_id
-        
-        #factscontact in posgres vs 
-        #
-        #
-        #              
+        def query_fact_contact
+            #contact_id > Leads > id
+            #creation > Leads > created_at
+            #company_name >Leads > email
+            #email> Leads > email
+            #project_name > Leads>project_name
+            self.conn.exec( "SELECT * FROM ")
+        end
+        def query_fact_elevator
+            
+            Elevator.all.each do |elevators|
+                serialnum =  elevators.serial_number
+                columnid = elevators.column_id
+                # batteryid = Column.columnid.battery_id
+                address = elevators.column.battery.building.address
+                puts columnid
+                date_of_commissioning = elevators.date_of_commissioning
+                puts date_of_commissioning
+                self.conn.exec("INSERT into factelevator (serial_number) VALUES (#{serialnum})")
+            end
+            #    Elevator.find_by_sql("SELECT id, serial_number FROM elevators").all.each do |elevators|
+            #     puts 'elevators'
+            #    end
+            # Elevator.pluck(:serial_number) do |elevators|
+            #     # puts elevators
+            #     # puts "good"
+            #     elevators.each do |row|
+            #         # puts row.values_at('serial_number')
+            #         # puts "great"
+            #         self.conn.exec("INSERT into factelevator (serial_number) VALUES (row)")
+            #         # puts elevators
+            #     end
+            # end
+            # Elevator.connection:serial_number.each do |elevators|
+            #     puts elevators
+            #     puts "wowiebazowie"
+            # end
+        end
+        def query_dim_customers
+            Customer.all.each do |customer|
+                creationdate =  customer.created_at
+                companyname = customer.company_name
+                # maincontact = customers.company_contact_full_name
+                mainemail = customer.company_contact_email
+                #customercity = 
+                nbelevators = 5
+                puts companyname
+                # puts maincontact
+                puts mainemail
+                puts nbelevators
+                self.conn.exec("INSERT into DimCustomers (company_name, creation) VALUES ('#{companyname.gsub("'", "")}', '#{creationdate}')")
+            end
+            
+            # self.conn.exec( "SELECT * FROM DimCustomers" ) do |result|
+            #     puts "hello"
+            #     #select answers from this table and for each, make one result
+            #     # creation = created_at
+            #     # company_name = company_name
+            #     # full_name_company_contact = company_contact_full_name
+            #     # email_company_contact = company_contact_email
+            #     # num_of_elevators = num entries in elevators table with user_id
+                
+            #     result.each do |row|
+            #         puts "wow"
+            #         #for each result do a row
+            #         puts row.values_at('company_name')
+            #         #look at the column with value 'company_name' and print those
+            #         self.conn.exec("INSERT into DimCustomers (company_name) VALUES ('DOG SHIT')")
+            #     end
+            # end
+        end
+
+        def empty_customer_table
+            self.conn.exec("TRUNCATE TABLE dimcustomers")
+        end
+        def empty_elevator_table
+            self.conn.exec("TRUNCATE TABLE factelevator")
+        end
+                    
         def sync_mysql
 
             self.sync_mysql_buildings
